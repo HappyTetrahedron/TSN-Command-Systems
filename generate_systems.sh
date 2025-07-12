33fe42bbdc124de373ee3ba1a47bb2de1f2fe2d2
Aline Abler <aline.abler@vshn.ch>
2025-07-12 14:11:23 +0200

    Generate jinja-compatible star system files

---
+++ generate_systems.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "Generating systems..."
+IFS=''
+for file in "XML Files/Star Systems/"*.xml
+do
+    PRINT=false
+    START=true
+    echo "  " $file
+    of="${file##*Star Systems/}"
+    while read -r line ; do
+        if $START
+        then
+            echo -n "{# "
+            START=false 
+        elif [[ $line == *"Coordinate X"* ]]
+        then
+            va="${line##*value=\"}"
+            echo -n x="${va%%\"*} "
+        elif [[ $line == *"Coordinate Y"* ]]
+        then
+            va="${line##*value=\"}"
+            echo -n y="${va%%\"*} "
+        elif [[ $line == *"Alignment"* ]]
+        then
+            va="${line##*value=\"}"
+            echo -n alignment="${va%%\"*} "
+        elif [[ $line == *"</start>"* ]]
+        then
+            echo "#}"
+            PRINT=true
+        elif [[ $line == *"</mission_data>"* ]]
+        then
+            echo "$file" 1>&2
+        elif $PRINT
+        then
+            echo "$line"
+        fi
+    done < "$file"  > "XML Files/Systems (Generated)/$of"
+done
